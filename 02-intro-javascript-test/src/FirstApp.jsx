5ea7fcd1d00ed76e8d351ffd053038e032fa55b2
Alexander <alexander.morenoc@outlook.com>
2022-08-18 17:52:46 -0500

    s5 64 getByText, toContain

--- 02-intro-javascript-test/src/FirstApp.jsx
+++ 02-intro-javascript-test/src/FirstApp.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types"
 
 export const FirstApp = ({title, subTitle, name}) =>
   <>
-    <h1>{title}</h1>
+    <h1> {title} </h1>
     <p>{subTitle}</p>
     <p>{name}</p>
   </>
--- 02-intro-javascript-test/test/FirstApp.test.jsx
+++ 02-intro-javascript-test/test/FirstApp.test.jsx
@@ -5,8 +5,20 @@ describe('Pruebas en <FirstApp />', () => {
   
   test('debe de hacer match con el snapshot', () => {
     const title = 'Hola, Soy Goku'
-    render( <FirstApp title={ title }/> )
-
+    const { container } = render( <FirstApp title={ title }/> );
+    
+    expect( container ).toMatchSnapshot();
   });
+
+
+  test('should mostrar el titulo en el h1', () => { 
+    const title = 'Hola, Soy Goku';
+    const { container, getByText } = render( <FirstApp title={ title }/> );
+    expect( getByText(title) ).toBeTruthy();
+
+    const h1 = container.querySelector('h1');
+    //con tobe si tenemos " Hola soy goku " (con espacios inicio final) da error asi solo validamos que contenga el texto con toContain
+    expect(h1.innerHTML).toContain(title);
+  })
   
 });
