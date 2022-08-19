f1b7919c4fa549737c13c9a18f48fe4b4a4a1851
Alexander <alexander.morenoc@outlook.com>
2022-08-18 22:18:11 -0500

    s5 67 simular eventos - click

--- 02-intro-javascript-test/src/CounterApp.jsx
+++ 02-intro-javascript-test/src/CounterApp.jsx
@@ -16,7 +16,7 @@ export const CounterApp =({value}) => {
       <h1>CounterApp</h1>
       <h2> { counter } </h2>
       <button onClick={handleSubtract}>-1</button>
-      <button onClick={handleReset}>Reset</button>
+      <button aria-label="btn-reset" onClick={handleReset}>Reset</button>
       <button onClick={handleAdd}>+1</button>
     </>
     
--- 02-intro-javascript-test/test/CounterApp.test.jsx
+++ 02-intro-javascript-test/test/CounterApp.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { CounterApp } from "../src/CounterApp";
 
 describe('Pruebas en el <CounterApp/>', () => {
@@ -14,6 +14,30 @@ describe('Pruebas en el <CounterApp/>', () => {
     render(<CounterApp value={ 100 }/>);
     expect( screen.getByText(100)).toBeTruthy();
     expect(screen.getByRole('heading',{ level:2 }).innerHTML ).toContain('100');
+  });
+
+  test('should incrementar con el boton +1', () => {
+    render(<CounterApp value={ initialValue }/>); 
+    fireEvent.click( screen.getByText('+1') );
+    expect( screen.getByText('11') ).toBeTruthy();
+  });
+
+  test('should decrementar con el boton -1', () => {
+    render(<CounterApp value={ initialValue }/>); 
+    fireEvent.click( screen.getByText('-1') );
+    expect( screen.getByText('9') ).toBeTruthy();
+  });
+
+  test('should funcionar el boton de reset', () => {
+    render(<CounterApp value={ initialValue }/>);
+    fireEvent.click( screen.getByText('+1') );
+    fireEvent.click( screen.getByText('+1') );
+    fireEvent.click( screen.getByText('+1') );
+    // fireEvent.click( screen.getByText('Reset') );
+    fireEvent.click(screen.getByRole('button',{name: 'btn-reset'} ));
+    // screen.debug();
+    expect( screen.getByText(initialValue) ).toBeTruthy();
+
   })
   
 });
