810a05096b955b55e2b8927249ed86802099afcb
Alexander <alexander.morenoc@outlook.com>
2022-08-25 17:17:38 -0500

    s9 profundizando Hooks / 121 useEffect

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { CounterApp } from './01-useState/CounterApp'
-import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { HooksApp } from './HooksApp'
 import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CounterWithCustomHook/>
-  </React.StrictMode>
+  // <React.StrictMode>
+    <SimpleForm/>
+  /* </React.StrictMode> */
 )
