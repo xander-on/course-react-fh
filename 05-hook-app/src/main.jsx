1af1406e46d428bc9d62aa98975aef5502cd3c0e
Alexander <alexander.morenoc@outlook.com>
2022-08-26 17:55:32 -0500

    S9 Profundizando Hooks / 133 useCallback

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -10,10 +10,11 @@ import ReactDOM from 'react-dom/client'
 // import { Layout } from './05-useLayoutEffect/Layout';
 // import { Memorize } from './06-memos/Memorize';
 import { MemoHook } from './06-memos/MemoHook';
+import { CallbackHook } from './06-memos/CallbackHook';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <MemoHook/>
+    <CallbackHook/>
   /* </React.StrictMode> */
 )
