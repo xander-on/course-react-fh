31aadfbe1868622219cfe62bf9c7425cb4e5260a
Alexander <alexander.morenoc@outlook.com>
2022-08-25 22:16:23 -0500

    S9 Profundizando Hooks / 127 useFetch + useCounter y optimizacion

---
+++ 05-hook-app/src/03-examples/Quote.jsx
@@ -0,0 +1,9 @@
+
+export const Quote = ({author, quote}) => {
+  return (
+    <blockquote className="blockquote text-end">
+      <p className="mb-2">{ quote }</p>
+      <footer className="blockquote-footer">{ author }</footer>
+    </blockquote>
+  )
+}
