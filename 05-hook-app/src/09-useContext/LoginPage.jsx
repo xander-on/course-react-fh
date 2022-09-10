aaf64724e3326aee6672bd78c82d715843f81b27
Alexander <alexander.morenoc@outlook.com>
2022-09-10 15:15:57 -0500

    s11 useContext / 161 createContext, contextProvider, useContext /  Fin de la seccion

--- 05-hook-app/src/09-useContext/LoginPage.jsx
+++ 05-hook-app/src/09-useContext/LoginPage.jsx
@@ -1,8 +1,25 @@
+import { useContext } from "react"
+import { UserContext } from "./context/UserContext";
+
 export const LoginPage = () => {
+
+  const { user, setUser } = useContext( UserContext );
+
   return (
     <>
       <h1>LoginPage</h1>
       <hr />
+
+      <pre>
+        { JSON.stringify( user, null, 3 )}
+      </pre>
+
+      <button 
+        onClick={ ()=> setUser({ id: 123, name: 'Tete', email: 'tete@gmail.com' }) }
+        className="btn btn-primary"
+      >
+        Establecer Usuario
+      </button>
     </>
   )
 }
--- 05-hook-app/src/09-useContext/MainApp.jsx
+++ 05-hook-app/src/09-useContext/MainApp.jsx
@@ -3,12 +3,13 @@ import { HomePage } from "./HomePage";
 import { AboutPage } from "./AboutPage"
 import { LoginPage } from "./LoginPage"
 import { Navbar } from "./Navbar";
+import { UserProvider } from "./context/UserProvider";
 
 
 
 export const MainApp = () => {
   return (
-    <>
+    <UserProvider>
       <h1>MainApp</h1>
       <Navbar/>
       <hr />
@@ -22,6 +23,6 @@ export const MainApp = () => {
         <Route path="/*" element={<Navigate to="/about"/>}/>
 
       </Routes>
-    </>
+    </UserProvider>
   )
 }
